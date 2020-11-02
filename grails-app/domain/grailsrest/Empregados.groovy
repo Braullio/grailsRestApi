8615200e3bc2da1b922657be405825912364948e
braullio <braulliovg@gmail.com>
2020-11-02 16:07:37 -0300

    grails create-domain-class empregados

---
+++ grails-app/domain/grailsrest/Empregados.groovy
@@ -0,0 +1,7 @@
+package grailsrest
+
+class Empregados {
+
+    static constraints = {
+    }
+}
---
+++ src/test/groovy/grailsrest/EmpregadosSpec.groovy
@@ -0,0 +1,18 @@
+package grailsrest
+
+import grails.testing.gorm.DomainUnitTest
+import spock.lang.Specification
+
+class EmpregadosSpec extends Specification implements DomainUnitTest<Empregados> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
